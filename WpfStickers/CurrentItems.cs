ff4bbc1d72d4a54fb5611e3a8c9bd206d6901b95
Евгений Нагуляк <jokersmilejd@hotmail.com>
2017-01-12 19:35:16 +0200

    before add BackgroundWorker

---
+++ WpfStickers/CurrentItems.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WpfStickers
+{
+    class CurrentItems
+    {
+        public string Db { get; set; }
+
+        public Task ParceCsv(string nameCsv)
+        {
+
+            return Task.Factory.StartNew(() => 
+            {
+                string[] values = File.ReadAllLines($"c:/Users/UBI-Note/Downloads/{nameCsv}");
+
+                var query = from line in values
+                            let data = line.Split(';')
+                            where isSticker(data[9])
+                            orderby Double.Parse(data[2]) / 100
+                            select new
+                            {
+                                name = data[11],
+                                sticker = data[9],
+                                price = Double.Parse(data[2]) / 100
+                            };
+
+                using (var file = new StreamWriter(@"D:\listStickers.txt"))
+                {
+                    foreach (var item in query)
+                    {
+                        Console.WriteLine($"{item.price}\t{item.name}\t{item.sticker}");
+                        file.WriteLine($"{item.price}\t{item.name}\t{item.sticker}");
+                    }
+                }
+
+                bool isSticker(string st)
+                {
+                    string[] idStickers = File.ReadAllLines(@"D:\Stikers2.txt");
+
+                    for (int i = 0; i < idStickers.Length; i++)
+                    {
+                        string[] stSplit = st.Split('|');
+
+                        foreach (var item in stSplit)
+                        {
+                            if (item.Equals(idStickers[i]))
+                            {
+                                return true;
+                            }
+                        }
+                    }
+
+                    return false;
+                }
+            });
+        }
+    }
+}
--- WpfStickers/MainWindow.xaml.cs
+++ WpfStickers/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
 
 namespace WpfStickers
 {
@@ -20,9 +22,80 @@ namespace WpfStickers
     /// </summary>
     public partial class MainWindow : Window
     {
+        CurrentItems items;
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private async void ShowButton_Click(object sender, RoutedEventArgs e)
+        {
+            showButton.IsEnabled = false;
+            testProgressBar.Minimum = 0;
+            testProgressBar.Maximum = 100;
+
+
+            using (var wc = new WebClient())
+            {
+                string jsonItems = await wc.DownloadStringTaskAsync(new Uri("https://market.csgo.com/itemdb/current_730.json"));
+                items = await Task.Factory.StartNew(() => JsonConvert.DeserializeObject<CurrentItems>(jsonItems));
+
+                wc.DownloadProgressChanged += Wc_DownloadProgressChanged;
+                wc.DownloadFileCompleted += Wc_DownloadFileCompletedAsync;
+
+                try
+                {
+                    wc.DownloadFileAsync(new Uri($"https://market.csgo.com/itemdb/{items.Db}"), $"c:/Users/UBI-Note/Downloads/{items.Db}");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    MessageBox.Show(items.Db);
+                }
+
+                //labelSpeed.Content = "Идет парсинг.";
+                //await items.ParceCsv(items.Db);
+                //labelSpeed.Content = "Парсинг завершен.";
+            }
+        }
+
+        private void Wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            //throw new NotImplementedException();
+
+            testProgressBar.Value = e.ProgressPercentage;
+            labelPercent.Content = e.ProgressPercentage.ToString() + "%";
+            labelSpeed.Content = "Идет загрузка...";
+            labelDownloaded.Content = string.Format("{0} MB's / {1} MB's",
+                (e.BytesReceived / 1024d / 1024d).ToString("0.00"),
+                (e.TotalBytesToReceive / 1024d / 1024d).ToString("0.00"));
+        }
+
+        private async void Wc_DownloadFileCompletedAsync(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            //throw new NotImplementedException();
+            showButton.IsEnabled = true;
+            testProgressBar.Value = 0;
+            labelSpeed.Content = "Загрузка завершена.";
+            labelPercent.Content = null;
+
+            labelSpeed.Content = "Идет парсинг.";
+            await items.ParceCsv(items.Db);
+            labelSpeed.Content = "Парсинг завершен.";
+        }
+
+        private void testButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (testLabel.Content.Equals(""))
+            {
+                testLabel.Content = "Magic!";
+            }
+            else
+            {
+                testLabel.Content = "";
+            }
+        }
+
+        
     }
 }
